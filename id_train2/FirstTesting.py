814de612df662f4423390466193853258f9f3ecf
Navin P <navinp1912@gmail.com>
2020-09-10 14:51:47 +0530

    Uploading imageai colab to github

---
+++ id_train2/FirstTesting.py
@@ -0,0 +1,15 @@
+from imageai.Prediction.Custom import CustomImagePrediction
+import os
+
+execution_path = os.getcwd()
+
+prediction = CustomImagePrediction()
+prediction.setModelTypeAsResNet()
+prediction.setModelPath("idenprof/models")
+prediction.setJsonPath("idenprof/json/model_class.json")
+prediction.loadModel(num_objects=3)
+
+predictions, probabilities = prediction.predictImage("image.jpg", result_count=3)
+
+for eachPrediction, eachProbability in zip(predictions, probabilities):
+        print(eachPrediction , " : " , eachProbability)
