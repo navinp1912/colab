6a23371c15d65d39e849dddb87a5de78a438b154
Navin P <navinp1912@gmail.com>
2020-09-11 16:08:58 +0530

    Chaning to squeezenet from densenet ,accuracy only 47%

--- id_train2/FirstTraining.py
+++ id_train2/FirstTraining.py
@@ -1,7 +1,7 @@
 from imageai.Prediction.Custom import ModelTraining
 
 model_trainer = ModelTraining()
-model_trainer.setModelTypeAsDenseNet()
+model_trainer.setModelTypeAsSqueezeNet()
 model_trainer.setDataDirectory("idenprof")
 #model_trainer.trainModel(num_objects=10, num_experiments=200, enhance_data=True, batch_size=32, show_network_summary=True)
 model_trainer.trainModel(num_objects=3, num_experiments=4, enhance_data=True, batch_size=32, show_network_summary=True)
